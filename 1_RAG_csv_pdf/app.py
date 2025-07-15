cb9f7a1f5063fb87c5bcd0373018f5da503f8106
aasim ayaz wani <46909571+aasimayazwani@users.noreply.github.com>
2025-07-15 13:52:32 -0700

    function added

--- 1_RAG_csv_pdf/app.py
+++ 1_RAG_csv_pdf/app.py
@@ -9,6 +9,7 @@ from langchain_groq import ChatGroq
 from langchain_openai import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.retrieval import create_retrieval_chain  # Added missing import
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import PyPDFLoader, CSVLoader
